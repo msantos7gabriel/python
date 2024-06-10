ebf756e1915f75addfa531657cd2c1484ee0c064
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-06-10 16:11:51 -0300

    reorganização e novas aulas

--- aulas/aula06.py
+++ aulas/aula06.py
@@ -1,4 +1,4 @@
 n1 = input('Primeiro número: ')
 n2 = input('Segundo número: ')
 n3 = n1 + n2
-print ('A Soma é: ', n3)
+print('A Soma é: ', n3)
---
+++ aulas/aula06a.py
@@ -0,0 +1,2 @@
+n1 = int(input('Digite Um Valor: '))
+print(type(n1))
