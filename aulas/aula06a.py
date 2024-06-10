2781f1974b8d95a0782c3349acfb480da14d1f5f
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-06-10 16:29:55 -0300

    aula 06

--- aulas/aula06a.py
+++ aulas/aula06a.py
@@ -1,2 +1,7 @@
-n1 = int(input('Digite Um Valor: '))
-print(type(n1))
+# n1 = int(input('Digite Um Valor: '))
+# print(type(n1))
+
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+n3 = n1+n2
+print('A Soma entre: {} e {} é: {}'.format(n1, n2, n3))
