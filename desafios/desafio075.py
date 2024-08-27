99fbbe72be5d5e801a06b24fec7776fa473a6909
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-08-26 22:14:38 -0300

    algumas atividades da aula 16

---
+++ desafios/desafio075.py
@@ -0,0 +1,16 @@
+valores = (int(input('Digite um numero: ')),
+           int(input('Digite um numero: ')),
+           int(input('Digite um numero: ')),
+           int(input('Digite um numero: ')))
+
+print(f'Você digitou os valores {valores}')
+print(f'O valor 9 apareceu {valores.count(9)} vezes')
+
+if 3 in valores:
+    print(f'O valor 3 apareceu na {valores.index(3)}ª posição')
+else:
+    print('O valor 3 não foi digitado em nenhuma posição')
+print('Os números pares digitados foram', end=' ')
+for i in range(0, len(valores)):
+    if valores[i] % 2 == 0:
+        print(valores[i], end=' ')
